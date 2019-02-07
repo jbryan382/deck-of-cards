1e530e698b1f062512fc3ce767d3b7e0ddd21556
Jordan Bryan <jbryan382@gmail.com>
2019-02-06 22:09:26 -0500

    Changed css around for look and attempted draw.

--- public/main.js
+++ public/main.js
@@ -1,7 +1,56 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let cards = [
+  'Ace',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'Jack',
+  'Queen',
+  'King'
+]
+
+let suit = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+
+let deck = []
+
+const buildDeck = () => {
+  for (let cardsvalue = 0; cardsvalue < cards.length; cardsvalue++) {
+    for (let suitvalue = 0; suitvalue < suit.length; suitvalue++) {
+      let cardText = cards[cardsvalue] + ' of ' + suit[suitvalue]
+      deck.push(cardText)
+    }
   }
+}
+
+const shuffleDeck = () => {
+  for (let i = 51; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[deck[i], deck[j]] = [deck[j], deck[i]]
+  }
+}
+
+const dealCard = event => {
+  for (let i = 51; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    let hand = deck.splice(j, 1)
+    document.querySelector('Hand').textContent = hand
+    document.querySelector('Hand').appendChild(hand)
+  }
+}
+
+const main = () => {
+  buildDeck()
+  shuffleDeck()
+
+  console.log(deck)
+
+  const button = document.querySelector('.draw')
+  button.addEventListener('click', dealCard)
 }
 
 document.addEventListener('DOMContentLoaded', main)
